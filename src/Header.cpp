c6fb0b57ac7742451cf1ff3b35dc131e91fbad2c
manearrior <mandrews@wpi.edu>
2013-03-16 10:11:56 -0400

    Moved files to include/ and src/

---
+++ src/Header.cpp
@@ -0,0 +1,16 @@
+/*#****************************************************************#*
+ * Empty Juice Voxel: Minecraft clone by the Empty Juice Box Group  *
+ * www              : http://www.juicebox.ckef-worx.com             *
+ * Copyright (c) Empty Juice Box Group :: All Rights Reserved       *
+ *#****************************************************************#*/
+#include <vector>
+
+#include "World.hpp"
+
+//EJV Namespace
+namespace EJV {
+
+
+
+
+}
